13c9aa689220dd43930338cf31eb1ff27856bb25
Chris <ccwijnia@gmail.com>
2025-04-03 22:18:56 +0700

    Add new routes and functionality to API handler

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,40 @@
+import { Hono } from "hono";
+import { getCookie } from "hono/cookie";
 import { handle } from "hono/vercel";
-import app from "../app";
+import { getVideo } from "../get-video";
+import { validateId } from "../get-position";
+import { content } from "../content";
+import Page from "../page";
+import { ThemeName } from "../render-board";
+
+const app = new Hono().basePath("/api");
+
+app.get("/", (c) => {
+  return c.redirect("/518");
+});
+
+app.get("/:id", async (ctx) => {
+  const themeName = getCookie(ctx, "themeName") as ThemeName | undefined;
+  const flipped =
+    (getCookie(ctx, "flipped") as "true" | "false" | undefined) || "false";
+  const video = await getVideo();
+
+  try {
+    const id = validateId(ctx.req.param("id"));
+
+    return ctx.html(
+      Page({
+        content,
+        flipped: flipped === "true",
+        id,
+        themeName: themeName || "merida",
+        video,
+      })
+    );
+  } catch (e) {
+    return ctx.redirect("/");
+  }
+});
 
 const handler = handle(app);
 
