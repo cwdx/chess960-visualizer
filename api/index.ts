8c2566e5ef1d3c0aa47fab56ad696d1e666e233b
Chris <ccwijnia@gmail.com>
2025-04-03 22:14:36 +0700

    Deleted bun.lockb, functions, and config.json

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,16 @@
+import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import app from "../app";
 
-export default handle(app);
+const app = new Hono().basePath("/api");
+
+app.get("/", (c) => {
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel" });
+});
+
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
