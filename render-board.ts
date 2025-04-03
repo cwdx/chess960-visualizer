7b2b05e8de52bbc7cb9bfa2b324dd90c90fe2d88
Chris <ccwijnia@gmail.com>
2025-04-03 21:25:15 +0700

    clean up

--- render-board.ts
+++ render-board.ts
@@ -1,80 +1,5 @@
-//RMBWKVNT
-//OPOPOPOP
-// / / / /
-/// / / /
-// / / / /
-/// / / /
-//popopopo
-//tnvqlbmr
-
-//tMvWlVmT
-//OoOoOoOo
-// + + + +
-//+ + + +
-// + + + +
-//+ + + +
-//pPpPpPpP
-//RnBqKbNr
-
-import fs from "fs";
-
-export const _renderFen = (fen: string): string => {
-  const piecePlacement = fen.split(" ")[0];
-  const ranks = piecePlacement.split("/");
-  const board = new Array(8).fill("").map(() => new Array(8).fill(""));
-
-  ranks.forEach((rank, i) => {
-    let squareIndex = 0;
-    for (let j = 0; j < rank.length; j++) {
-      const char = rank[j];
-      const len = parseInt(char);
-      if (isNaN(len)) {
-        console.log("char", char);
-      } else {
-        console.log("len", len);
-
-        for (let k = 0; k < len; k++) {
-          console.log("k", k);
-        }
-      }
-    }
-  });
-
-  return board.map((row) => row.join("")).join("\n");
-};
-
-const defaultFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-
-export const oldRenderFen = (fen: string): string => {
-  const piecePlacement = fen.split(" ")[0];
-  const ranks = piecePlacement.split("/");
-  const board = new Array(8).fill("").map(() => new Array(8).fill(""));
-
-  // Process each rank
-  for (let rankIndex = 0; rankIndex < ranks.length; rankIndex++) {
-    const rank = ranks[rankIndex];
-    let squareIndex = 0;
-
-    for (let i = 0; i < rank.length; i++) {
-      const char = rank[i];
-      if (isNaN(parseInt(char))) {
-        board[rankIndex][squareIndex] = template[char][i % 2 === 1 ? 0 : 1];
-        squareIndex++;
-      } else {
-        const emptyCount = parseInt(char);
-        for (let j = 0; j < emptyCount; j++) {
-          board[rankIndex][squareIndex] = template.e[i % 2 === 1 ? 0 : 1];
-          squareIndex++;
-        }
-      }
-    }
-  }
-
-  return board
-    .map((row) => row.join(""))
-    .join("\n")
-    .trim();
-};
+export const defaultFen =
+  "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
 function renderFen(fen: string) {
   const boardPart = fen.split(" ")[0];
@@ -150,23 +75,23 @@ const lucenaTheme: Theme = {
 
 const utrechtTheme: Theme = {
   // Black pieces
-  k: ["L", "K"], // Black king: odd = "L" (black square), even = "K" (white square)
-  q: ["W", "Q"], // Black queen: odd = "W", even = "Q"
-  r: ["T", "R"], // Black rook: odd = "T", even = "R"
-  b: ["V", "B"], // Black bishop: odd = "V", even = "B"
-  n: ["M", "N"], // Black knight: odd = "M", even = "N"
-  p: ["O", "P"], // Black pawn: odd = "O", even = "P"
+  k: ["L", "K"],
+  q: ["W", "Q"],
+  r: ["T", "R"],
+  b: ["V", "B"],
+  n: ["M", "N"],
+  p: ["O", "P"],
 
-  // White pieces
-  K: ["l", "k"], // White king: odd = "l", even = "k"
-  Q: ["w", "q"], // White queen: odd = "w", even = "q"
-  R: ["t", "r"], // White rook: odd = "t", even = "r"
-  B: ["v", "b"], // White bishop: odd = "v", even = "b"
-  N: ["m", "n"], // White knight: odd = "m", even = "n"
-  P: ["o", "p"], // White pawn: odd = "o", even = "p"
+  // White piece
+  K: ["l", "k"],
+  Q: ["w", "q"],
+  R: ["t", "r"],
+  B: ["v", "b"],
+  N: ["m", "n"],
+  P: ["o", "p"],
 
   // Empty square
-  "+": ["/", " "], // odd = "/" (black square), even = " " (white square)
+  "+": ["/", " "],
 };
 
 function applyTheme(renderedFen: string, themeMap: Theme, flipped = true) {
@@ -200,30 +125,6 @@ function applyTheme(renderedFen: string, themeMap: Theme, flipped = true) {
   }
 }
 
-const expectedTheme = `tMvWlVmT
-OoOoOoOo
- + + + +
-+ + + + 
- + + + +
-+ + + + 
-pPpPpPpP
-RnBqKbNr`;
-
-// onst rendered = renderFen(defaultFen);
-// / console.log(rendered);
-// / console.log("\n");
-// onst theme = applyTheme(rendered, utrechtTheme);
-// onsole.log(theme);
-// onsole.log("\n");
-// onsole.log(expectedTheme);
-//
-// onsole.log("equal", theme === expectedTheme);
-
-//console.log("new", renderFen(defaultFen));
-
-// read file with bun sync
-export const miscelKitchenSink = fs.readFileSync("unicode.txt", "utf8");
-
 export const themes = {
   merida: meridaTheme,
   utrecht: utrechtTheme,
@@ -247,7 +148,11 @@ export const themes = {
 
 export type ThemeName = keyof typeof themes;
 
-export function renderBoard(fen: string, themeName: ThemeName, flipped = false) {
+export function renderBoard(
+  fen: string,
+  themeName: ThemeName,
+  flipped = false
+) {
   const rendered = renderFen(fen);
   const theme = applyTheme(rendered, themes[themeName], flipped);
   return theme;
