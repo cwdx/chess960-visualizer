97c327dbebf0b04c1e3de148bb3a43a8605a7756
Chris <ccwijnia@gmail.com>
2025-04-04 09:09:01 +0700

    Update server configuration and start scripts for Bun CLI

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import app from "./utils/app";
 
-export default {
-  port: process.env.PORT || 3000,
+const PORT = Number(process.env.PORT || 8081);
+const NODE_ENV = process.env.NODE_ENV ?? "development";
+
+const server = Bun.serve({
+  port: PORT,
   fetch: app.fetch,
-};
+});
+
+console.log(`[${NODE_ENV}] Serving http://localhost:${server.port}`);
